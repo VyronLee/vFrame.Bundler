256ad470e88ab430ab7bbac595758510865ea661
VyronLee <lwz_jz@hotmail.com>
2020-01-03 16:06:39 +0800

    Add projector extensions.

---
+++ vFrame.Bundler/Extension/ProjectorExtension.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using vFrame.Bundler.Interface;
+
+namespace vFrame.Bundler.Extension
+{
+    public static class ProjectorExtension
+    {
+        public static void SetMaterial(this Projector target, IAsset asset)
+        {
+            asset.SetTo(target, "material");
+        }
+    }
+}
