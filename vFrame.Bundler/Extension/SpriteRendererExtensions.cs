76c584a24ca690ce54a760177469fe132d5685b7
VyronLee <lwz_jz@hotmail.com>
2020-05-21 09:24:47 +0100

    Add sprite renderer extensions.

---
+++ vFrame.Bundler/Extension/SpriteRendererExtensions.cs
@@ -0,0 +1,22 @@
+﻿//------------------------------------------------------------
+//        File:  SpriteRendererExtensions.cs
+//       Brief:  SpriteRendererExtensions
+//
+//      Author:  VyronLee, lwz_jz@hotmail.com
+//
+//    Modified:  2020-05-21 16:20
+//   Copyright:  Copyright (c) 2020, VyronLee
+//============================================================
+using UnityEngine;
+using vFrame.Bundler.Interface;
+
+namespace vFrame.Bundler.Extension
+{
+    public static class SpriteRendererExtensions
+    {
+        public static void SetSprite(this SpriteRenderer target, IAsset asset)
+        {
+            asset.SetTo(target, "sprite");
+        }
+    }
+}
