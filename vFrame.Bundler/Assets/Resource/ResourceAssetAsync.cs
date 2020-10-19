3b4dc9e8df3c46c2b37f5d3d9869c4ed222e7a78
VyronLee <lwz_jz@hotmail.com>
2020-10-19 13:08:49 +0100

    Simulate async operation while using AssetDatabase.LoadAssetAtPath.

--- vFrame.Bundler/Assets/Resource/ResourceAssetAsync.cs
+++ vFrame.Bundler/Assets/Resource/ResourceAssetAsync.cs
@@ -16,8 +16,9 @@ using vFrame.Bundler.Interface;
 using vFrame.Bundler.Loaders;
 using vFrame.Bundler.Utils;
 using vFrame.Bundler.Utils.Pools;
-using Logger = vFrame.Bundler.Logs.Logger;
 using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -26,7 +27,7 @@ namespace vFrame.Bundler.Assets.Resource
 {
     public sealed class ResourceAssetAsync : AssetBase, IAssetAsync
     {
-        private ResourceRequest _request;
+        private ResourceAsyncRequest _request;
 
         public ResourceAssetAsync(string assetName, Type type, BundleLoaderBase target = null) : base(assetName, type,
             target)
@@ -38,7 +39,7 @@ namespace vFrame.Bundler.Assets.Resource
             if (_asset)
                 return false;
 
-            if (_request == null || !_request.isDone)
+            if (_request == null || _request.keepWaiting)
                 return true;
 
             _asset = _request.asset;
@@ -76,27 +77,94 @@ namespace vFrame.Bundler.Assets.Resource
 #if UNITY_EDITOR
             if (BundlerCustomSettingsInEditorMode.kUseAssetDatabaseInsteadOfResources)
             {
-                _asset = AssetDatabase.LoadAssetAtPath(_path, _type);
-                if (!_asset)
-                    throw new BundleAssetLoadFailedException(
-                        string.Format("Cannot load asset {0} from AssetDatabase: ", _path));
+                _request = new AssetDatabaseAsync();
+                _request.LoadAssetAtPath(_path, _type);
                 return;
             }
 #endif
 
-            var resPath = PathUtility.RelativeProjectPathToRelativeResourcesPath(_path);
-
-            var sb = StringBuilderPool.Get();
-            sb.Append(Path.GetDirectoryName(resPath));
-            sb.Append("/");
-            sb.Append(Path.GetFileNameWithoutExtension(resPath));
-
-            _request = Resources.LoadAsync(sb.ToString(), _type);
-            if (_request == null)
-                throw new BundleAssetLoadFailedException(
-                    string.Format("Cannot load asset {0} from resources: ", sb));
-
-            StringBuilderPool.Return(sb);
+            _request = new ResourceAsync();
+            _request.LoadAssetAtPath(_path, _type);
         }
+
+        private abstract class ResourceAsyncRequest : CustomYieldInstruction
+        {
+            public abstract float progress { get; }
+            public abstract Object asset { get; }
+            public abstract void LoadAssetAtPath(string path, Type type);
+        }
+
+        private class ResourceAsync : ResourceAsyncRequest
+        {
+            private ResourceRequest _request;
+
+            public override bool keepWaiting {
+                get { return !_request.isDone; }
+            }
+
+            public override float progress {
+                get { return _request.progress; }
+            }
+
+            public override Object asset {
+                get { return _request.asset; }
+            }
+
+            public override void LoadAssetAtPath(string path, Type type) {
+                var resPath = PathUtility.RelativeProjectPathToRelativeResourcesPath(path);
+
+                var sb = StringBuilderPool.Get();
+                sb.Append(Path.GetDirectoryName(resPath));
+                sb.Append("/");
+                sb.Append(Path.GetFileNameWithoutExtension(resPath));
+
+                _request = Resources.LoadAsync(sb.ToString(), type);
+                if (_request == null)
+                    throw new BundleAssetLoadFailedException(
+                        string.Format("Cannot load asset {0} from resources: ", sb));
+
+                StringBuilderPool.Return(sb);
+            }
+        }
+
+#if UNITY_EDITOR
+        private class AssetDatabaseAsync : ResourceAsyncRequest
+        {
+            private int _startFrame;
+            private int _frameLength;
+
+            private string _path;
+            private Type _type;
+
+            public override bool keepWaiting {
+                get { return Time.frameCount - _startFrame < _frameLength; }
+            }
+
+            public override float progress {
+                get { return Mathf.Min((float) (Time.frameCount - _startFrame) / _frameLength, 1f); }
+            }
+
+            public override void LoadAssetAtPath(string path, Type type) {
+                _path = path;
+                _type = type;
+                _startFrame = Time.frameCount;
+                _frameLength = Random.Range(6, 30);
+            }
+
+            public override Object asset {
+                get {
+                    if (keepWaiting) {
+                        return null;
+                    }
+
+                    var obj = AssetDatabase.LoadAssetAtPath(_path, _type);
+                    if (!obj)
+                        throw new BundleAssetLoadFailedException(
+                            string.Format("Cannot load asset {0} from AssetDatabase: ", _path));
+                    return obj;
+                }
+            }
+        }
+#endif
     }
 }
