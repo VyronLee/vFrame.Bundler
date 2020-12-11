42129436f759b8e57e2c8a0316fb7b3b4d5527d2
VyronLee <lwz_jz@hotmail.com>
2020-12-11 04:01:23 +0000

    Edit build scripts.

--- BuildAllPlatform.bat
+++ BuildAllPlatform.bat
@@ -4,24 +4,14 @@ SET CURDIR=%~dp0
 
 echo Building for platform: Editor
 msbuild vFrame.Bundler.sln /t:Clean,Rebuild /p:Configuration=Debug /p:Platform="Any CPU" /p:DefineConstants="TRACE DEBUG UNITY_EDITOR"
-if not exist "%CURDIR%\Output\vFrame.Bundler\Runtime" mkdir %CURDIR%\Output\vFrame.Bundler\Runtime
-copy /Y %CURDIR%\Build\vFrame.Bundler\Debug\vFrame.Bundler.* %CURDIR%\Output\vFrame.Bundler\Runtime\
+if not exist "%CURDIR%\Output\vFrame.Bundler\Runtime\Debug" mkdir %CURDIR%\Output\vFrame.Bundler\Runtime\Debug
+copy /Y %CURDIR%\Build\vFrame.Bundler\Debug\vFrame.Bundler.* %CURDIR%\Output\vFrame.Bundler\Runtime\Debug
 if not exist "%CURDIR%\Output\vFrame.Bundler\Editor" mkdir %CURDIR%\Output\vFrame.Bundler\Editor
 copy /Y %CURDIR%\Build\vFrame.Bundler.Editor\Debug\vFrame.Bundler.Editor.* %CURDIR%\Output\vFrame.Bundler\Editor\
 
-echo Building for platform: Standalone
-msbuild vFrame.Bundler.sln /t:Clean,Rebuild /p:Configuration=Release /p:Platform="Any CPU" /p:DefineConstants="TRACE UNITY_STANDALONE"
-if not exist "%CURDIR%\Output\vFrame.Bundler\Runtime\Standalone" mkdir %CURDIR%\Output\vFrame.Bundler\Runtime\Standalone
-copy /Y %CURDIR%\Build\vFrame.Bundler\Release\vFrame.Bundler.* %CURDIR%\Output\vFrame.Bundler\Runtime\Standalone\
-
-echo Building for platform: Android
-msbuild vFrame.Bundler.sln /t:Clean,Rebuild /p:Configuration=Release /p:Platform="Any CPU" /p:DefineConstants="TRACE UNITY_ANDROID"
-if not exist "%CURDIR%\Output\vFrame.Bundler\Runtime\Android" mkdir %CURDIR%\Output\vFrame.Bundler\Runtime\Android
-copy /Y %CURDIR%\Build\vFrame.Bundler\Release\vFrame.Bundler.* %CURDIR%\Output\vFrame.Bundler\Runtime\Android\
-
-echo Building for platform: iOS
-msbuild vFrame.Bundler.sln /t:Clean,Rebuild /p:Configuration=Release /p:Platform="Any CPU" /p:DefineConstants="TRACE UNITY_IOS"
-if not exist "%CURDIR%\Output\vFrame.Bundler\Runtime\iOS" mkdir %CURDIR%\Output\vFrame.Bundler\Runtime\iOS
-copy /Y %CURDIR%\Build\vFrame.Bundler\Release\vFrame.Bundler.* %CURDIR%\Output\vFrame.Bundler\Runtime\iOS\
+echo Building for platform: Runtime
+msbuild vFrame.Bundler.sln /t:Clean,Rebuild /p:Configuration=Release /p:Platform="Any CPU" /p:DefineConstants="TRACE"
+if not exist "%CURDIR%\Output\vFrame.Bundler\Runtime\Release" mkdir %CURDIR%\Output\vFrame.Bundler\Runtime\Release
+copy /Y %CURDIR%\Build\vFrame.Bundler\Release\vFrame.Bundler.* %CURDIR%\Output\vFrame.Bundler\Runtime\Release
 
 Pause
