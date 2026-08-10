361050e8ab4aabe1e9f52077d8b3d0d2f44a624d
VyronLee <lwz_jz@hotmail.com>
2026-08-10 21:23:03 +0800

    test(bundler): cover aborted group parent release

--- Assets/vFrame.Bundler/Tests/EditMode/AssetBundleLoaderGroupAbortTests.cs
+++ Assets/vFrame.Bundler/Tests/EditMode/AssetBundleLoaderGroupAbortTests.cs
@@ -27,6 +27,18 @@ namespace vFrame.Bundler.Tests.EditMode
             public void LogException(System.Exception exception) { }
         }
 
+        private sealed class ParentLoader : Loader
+        {
+            public ParentLoader(BundlerContexts contexts)
+                : base(contexts, new LoaderContexts()) { }
+
+            public override float Progress => 1f;
+            protected override void OnStart() { }
+            protected override void OnStop() { }
+            protected override void OnUpdate() { }
+            protected override void OnForceComplete() { }
+        }
+
         [Test]
         public void CollectionShutdown_MissingAssetMetadata_DoesNotThrow() {
             var manifest = new BundlerManifest();
@@ -40,6 +52,54 @@ namespace vFrame.Bundler.Tests.EditMode
             manifest.Assets.Add(AssetPath, BundlePath);
 
             AssertAbortPathCanBeDestroyedByCollection(manifest);
+        }
+
+        [Test]
+        public void Destroy_MissingAssetMetadata_ReleasesRetainedParent() {
+            var manifest = new BundlerManifest();
+            var options = new BundlerOptions {
+                LogHandler = new SilentLogHandler()
+            };
+            var facade = new Bundler(manifest, options);
+            var contexts = new BundlerContexts {
+                Bundler = facade,
+                Manifest = manifest,
+                Options = options
+            };
+
+            var parent = new ParentLoader(contexts);
+            var loader = new AssetBundleLoaderGroupSync(contexts, new LoaderContexts {
+                AssetPath = AssetPath,
+                ParentLoader = parent
+            });
+            var destroyAttempted = false;
+
+            try {
+                Assert.That(parent.References, Is.EqualTo(1),
+                    "Loader construction must retain its pipeline parent");
+
+                destroyAttempted = true;
+                Assert.DoesNotThrow(() => loader.Destroy(),
+                    "aborted group teardown must not fail before Loader.OnDestroy");
+
+                Assert.That(parent.References, Is.EqualTo(0),
+                    "aborted group teardown must reach Loader.OnDestroy and release its parent");
+            }
+            finally {
+                try {
+                    if (!destroyAttempted) {
+                        loader.Destroy();
+                    }
+                }
+                finally {
+                    try {
+                        parent.Destroy();
+                    }
+                    finally {
+                        facade.Destroy();
+                    }
+                }
+            }
         }
 
         private static void AssertAbortPathCanBeDestroyedByCollection(BundlerManifest manifest) {
